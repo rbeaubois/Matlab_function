331b89cbcb4de09313fb44735b79b194a446be48
rbeaubois <captain.ulxus@gmail.com>
2020-10-08 16:31:54 +0200

    Simplified script ptut

--- ptut.m
+++ ptut.m
@@ -19,27 +19,27 @@ save_param      = struct( ...
     'fig',  save_fig ...
 );
 
-% Raster plot of one file
-if strcmp(binf_get_type, 'one')
-    bin_trace_analysis(bin_fpath, trace_time, save_param);
-% Raster plot of all files
-elseif strcmp(binf_get_type, 'all')
-    for i = 1:nb_binf
-        bin_trace_analysis(bin_fpath(i), trace_time, save_param);
-    end
-end
+% % Raster plot of one file
+% if strcmp(binf_get_type, 'one')
+%     bin_trace_analysis(bin_fpath, trace_time, save_param);
+% % Raster plot of all files
+% elseif strcmp(binf_get_type, 'all')
+%     for i = 1:nb_binf
+%         bin_trace_analysis(bin_fpath(i), trace_time, save_param);
+%     end
+% end
 
-    % Read binary file
-        [Signal, fname_no_ext, rec_param]           = read_bin(bin_fpath, trace_time);   % Signals of electrodes + name of file + recording parameters
+% Read binary file
+    [Signal, fname_no_ext, rec_param]           = read_bin(bin_fpath, trace_time);   % Signals of electrodes + name of file + recording parameters
 
-    % Filter signal
-        [LP_Signal_fix, HP_Signal_fix, time_ms]     = filter_signal(rec_param.fs, rec_param.nb_chan, Signal);
+% Filter signal
+    [LP_Signal_fix, HP_Signal_fix, time_ms]     = filter_signal(rec_param.fs, rec_param.nb_chan, Signal);
 
 %% Spike detection
     visual_on=0;
     magnification=5; % magnification *STDEV
 
-%////////////
+%% Compute analysis
     [All_spikes_pos, All_spikes_neg, ...
      Mean_posspks_amp, Mean_negspks_amp, ... 
      Num_posspks, Num_negspks, ...

a8fda86110620c08dfde3ec14bd81901b50721ae
rbeaubois <captain.ulxus@gmail.com>
2020-10-09 11:06:13 +0200

    Update gitignore

--- bin2mat.m
+++ bin2mat.m
@@ -19,15 +19,15 @@ save_param      = struct( ...
     'fig',  save_fig ...
 );
 
-% % Raster plot of one file
-% if strcmp(binf_get_type, 'one')
-%     bin_trace_analysis(bin_fpath, trace_time, save_param);
-% % Raster plot of all files
-% elseif strcmp(binf_get_type, 'all')
-%     for i = 1:nb_binf
-%         bin_trace_analysis(bin_fpath(i), trace_time, save_param);
-%     end
-% end
+% Raster plot of one file
+if strcmp(binf_get_type, 'one')
+    bin_trace_analysis(bin_fpath, trace_time, save_param);
+% Raster plot of all files
+elseif strcmp(binf_get_type, 'all')
+    for i = 1:nb_binf
+        bin_trace_analysis(bin_fpath(i), trace_time, save_param);
+    end
+end
 
 % Read binary file
     [Signal, fname_no_ext, rec_param]           = read_bin(bin_fpath, trace_time);   % Signals of electrodes + name of file + recording parameters
